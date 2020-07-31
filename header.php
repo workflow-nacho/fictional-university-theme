14383c2407bbef79d61d32ced2e3f06eb946a4df
Nacho <joseignaciorm13@gmail.com>
2020-07-31 18:01:59 +0200

    Section 8: Displaying CPT Event, created  single-event and archive-event

--- header.php
+++ header.php
@@ -25,10 +25,10 @@
             <ul>
               <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 6) echo 'class="current-menu-item"'; ?> ><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+              <li <?php if (get_post_type() == 'event') echo 'class="current-menu-item"'; ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
-            </ul>-->
+            </ul>
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
