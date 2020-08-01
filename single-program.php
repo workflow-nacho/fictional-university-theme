f7a913f9863739a6edf787d2d3af1fbeaade0b34
José Ignacio Rondón Medina <joseignaciorm@iMac-de-Jose.local>
2020-08-01 19:28:00 +0200

    Section 6 CPT Program: Displaying Relationships between program and event CPT on (Front-End).

--- single-program.php
+++ single-program.php
@@ -1,4 +1,3 @@
-
 <?php get_header() ?>
 <div class="page-banner">
 	<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( 'images/ocean.jpg' ) ?>);"></div>
@@ -17,10 +16,56 @@
 		<p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program') ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title() ?></span></p>
 	</div>
 
-    <div class="generic-content">
-        <?php the_content() ?>
-    </div>
-<?php } ?> 
+	<div class="generic-content"><?php the_content() ?></div>
+	
+	<?php
+		$today = date('Ymd');
+		$upcommingEvent = new WP_Query(array(
+				'posts_per_page' => 2,
+				'post_type' => 'event',
+				'meta_key' => 'event_date',
+				'orderby' => 'meta_value_num',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'event_date',
+						'compare' => '>=',
+						'value' => $today,
+						'type' => 'numeric'
+					),
+					array(
+						'key' => 'related_programs', // array(12, 120, 1250); -> WP serialize in DB -> a:3:{i:'0'; i:"12"; ...}
+						'compare' => 'LIKE',
+						'value' => '"' . get_the_ID() . '"', // "12"
+					)
+				)
+			));
+		if ($upcommingEvent->have_posts()) {
+			echo '<hr class="section-break">';
+		echo '<h2 class="headline headline--medium">Upcomming ' . get_the_title() . ' Events</h2>';
+		while ($upcommingEvent->have_posts()) {
+			$upcommingEvent->the_post(); ?>
+			<div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
+                  <span class="event-summary__month"><?php
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                  ?></span>
+                  <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+                  <p><?php if (has_excerpt()) {
+                        echo get_the_excerpt();
+                    } else {
+                        echo wp_trim_words(get_the_content(), 18);
+                    } ?><a href="<?php the_permalink() ?>" class="nu gray"> Learn more</a></p>
+                </div>
+              </div>
+		 <?php } ?>
+
+<?php }
+} ?> 
 </div>
 
 <?php get_footer() ?>
