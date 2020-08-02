8c091627844a89993e3ab203a30136ea2265dd89
Nacho <joseignaciorm13@gmail.com>
2020-08-02 13:00:21 +0200

    Feature image sizes cropping: added new theme features for images. Added plugins: regenerate images and manual image crop

--- functions.php
+++ functions.php
@@ -20,9 +20,11 @@ add_action('wp_enqueue_scripts', 'university_files');
  * THEME FEATURES
  */
 function university_features() {
-    #register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
+    add_image_size( 'professorLandscape', '400', '260', true ); # Nickname, width, height, crop.
+    add_image_size( 'professorPortrait', '480', '650', true );
+    add_image_size( 'pageBanner', 1500, 350, true );
 }
 
 add_action('after_setup_theme', 'university_features');
