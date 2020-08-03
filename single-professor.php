871e8c08158f99cedb16c05a0c6ff75a66a2a519
José Ignacio Rondón Medina <joseignaciorm@iMac-de-Jose.local>
2020-08-03 10:03:27 +0200

    Added closing tag for content of the single professor post

--- single-professor.php
+++ single-professor.php
@@ -1,22 +1,24 @@
 
 <?php get_header() ?>
 
-<?php while(have_posts()) {
+<?php 
+while(have_posts()) {
     the_post(); 
     pageBanner();
 ?>
-    <div class="container container--narrow page-section">
 
-        <div class="generic-content">
-            <div class="row group">
-                <div class="one-third">
-                    <?php the_post_thumbnail('professorPortrait'); ?>
-                </div>
-                <div class="two-thirds">
-                    <?php the_content(); ?>
-                </div>
+<div class="container container--narrow page-section">
+
+    <div class="generic-content">
+        <div class="row group">
+            <div class="one-third">
+                <?php the_post_thumbnail('professorPortrait'); ?>
+            </div>
+            <div class="two-thirds">
+                <?php the_content(); ?>
             </div>
         </div>
+    </div>
 	
 	<?php
 	$relatedPrograms = get_field('related_programs');
@@ -24,16 +26,17 @@
 		echo '<hr class="section-break">';
 		echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
 		echo '<ul class="link-list min-list">';
-		foreach ($relatedPrograms as $program) {
-	?>
-		<li><a href="<?php echo get_the_permalink($program) ?>"><?php echo get_the_title($program); ?></a></li>
+		foreach ($relatedPrograms as $program) { 
+    ?>
+
+		    <li><a href="<?php echo get_the_permalink($program) ?>"><?php echo get_the_title($program); ?></a></li>
+
 	<?php } # EndForeach
 		echo '</ul>'; 
-	} 
-	?>
-
+	} ?>
 
 <?php } # End while loop ?> 
-</>
 
-<?php get_footer() ?>
+</div>
+
+<?php get_footer(); ?>
