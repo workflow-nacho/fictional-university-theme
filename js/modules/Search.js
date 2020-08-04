49e5e11e23a80d29e27de1f71f3a7294381266c3
Nacho <joseignaciorm13@gmail.com>
2020-08-04 11:25:57 +0200

    Managing time to only send search request to WP server when user stops typing

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,8 +6,10 @@ class Search {
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
 
   // 2. events
@@ -15,9 +17,19 @@ class Search {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
 
   // 3. methods (function, action...)
+  // Managing time to send search request to WP server when user stops typing.
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function () {
+      console.log("This is a timeout test");
+    }, 2000);
+  }
+
+  // Open and close Search Overlay pushing key "ESC" to close it and key "S" to close it.
   keyPressDispatcher(e) {
     // s(keyCode)=83
     if (e.keyCode == 83 && !this.isOverlayOpen) {
@@ -30,12 +42,14 @@ class Search {
     }
   }
 
+  // To open Search Overlay and to hide vertical scroll
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
     this.isOverlayOpen = true;
   }
 
+  // To close Search Overlay and to show vertical scroll
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
