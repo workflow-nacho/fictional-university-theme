64eabf4415dda16a1d84b5be97bb98e0368f95b0
José Ignacio Rondón Medina <joseignaciorm@iMac-de-Jose.local>
2020-08-07 09:39:20 +0200

    Updating functions.php file

--- functions.php
+++ functions.php
@@ -62,8 +62,8 @@ if (!function_exists('university_files')) {
             wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true); # This only work in our local machine. Not on a production server.
         } else {
                 wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.9678b4003190d41dd438.js'), NULL, '1.0', true);
-                wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.242f5822405183f32bbe.js'), NULL, '1.0', true);
-                wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.242f5822405183f32bbe.css'));
+                wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.92adb989ebddceaaa1fb.js'), NULL, '1.0', true);
+                wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.92adb989ebddceaaa1fb.css'));
             }  
         
         // Function that will output JavaScript Data intohtml source of the WebPage
