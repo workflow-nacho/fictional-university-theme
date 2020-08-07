6648751450183af69424949264de9d7a29bce93e
Nacho <joseignaciorm13@gmail.com>
2020-08-07 16:22:51 +0200

    Added array meta_query relation => or...

--- inc/search-route.php
+++ inc/search-route.php
@@ -57,7 +57,8 @@ if (!function_exists('universitySearchResults')) {
             if (get_post_type() == 'program') {
                 array_push($results['programs'], array(
                     'title' => get_the_title(),
-                    'permalink' => get_the_permalink()
+                    'permalink' => get_the_permalink(),
+                    'id' => get_the_ID()
                 ));
             }
 
@@ -87,6 +88,44 @@ if (!function_exists('universitySearchResults')) {
                 ));
             }
         }
+
+        /**
+         * SECOND SEARCH QUERY BASED ON RELATIONSHIP
+         */
+        $programRelationshipQuery = new WP_Query(array(
+            'post_type' => 'professor',
+            'meta_query' => array(
+                array('relation' => 'OR'),
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . $results['programs'][0]['id'] . '"'
+                ),
+                /*array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . $results['programs'][0]['id'] . '"'
+                ),
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . $results['programs'][0]['id'] . '"'
+                )*/
+            )
+        ));
+
+        while ($programRelationshipQuery->have_posts()) {
+            $programRelationshipQuery->the_post();
+            if (get_post_type() == 'professor') {
+                array_push($results['professors'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                ));
+            }
+        }
+
+        $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
 
         return $results;
     }
