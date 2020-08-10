2b63b90cb4885c853dde472bca11b23914e6ea86
Nacho <joseignaciorm13@gmail.com>
2020-08-10 13:01:33 +0200

    Section 19.2: Connecting with REST API, created dynamic DELETE functionality in POST notes.

---
+++ js/modules/MyNotes.js
@@ -0,0 +1,35 @@
+import $ from "jquery";
+
+class MyNotes {
+  constructor() {
+    this.events();
+  }
+
+  events() {
+    $(".delete-note").on("click", this.deleteNote);
+  }
+
+  // Custom Methods will go here
+  deleteNote(e) {
+    var thisNote = $(e.target).parents("li");
+    $.ajax({
+      url:
+        universityData.root_url + "/wp-json/wp/v2/note/" + thisNote.data("id"),
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
+      type: "DELETE",
+      success: (response) => {
+        thisNote.slideUp();
+        console.log("Congrats");
+        console.log(response);
+      },
+      error: (response) => {
+        console.log("Sorry");
+        console.log(response);
+      },
+    });
+  }
+}
+
+export default MyNotes;
