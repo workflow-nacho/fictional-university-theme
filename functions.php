809bead4d1378bf33f3061120f18956195b37bee
Nacho <joseignaciorm13@gmail.com>
2020-08-11 11:53:53 +0200

    Section 19.6 - Note Permissions and Security (Part 2): Sanitizing input and textarea content for subscriber post note)

--- functions.php
+++ functions.php
@@ -231,6 +231,11 @@ add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 if (!function_exists('makeNotePrivate')) {
     function makeNotePrivate ($data) {
+        if ($data['post_type'] == 'note') {
+            $data['post_title'] = sanitize_text_field( $data['post_title'] );
+            $data['post_content'] = sanitize_textarea_field( $data['post_content'] );
+        }
+
         if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
             $data['post_status'] = 'private';
         }
